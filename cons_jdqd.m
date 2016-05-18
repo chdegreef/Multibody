2b10a64292ff2072bef8aa3696d56e580b611cb0
Chdegreef <christophe.degreef@student.uclouvain.be>
2016-05-18 18:34:41 +0200

    Avec hJ et jdqd

---
+++ cons_jdqd.m
@@ -0,0 +1,11 @@
+function jdqd = cons_jdqd(data)
+l_arm = 0.5;
+h_knuckle = 0.25;
+
+q = data.q;
+qd = data.qd;
+jdqd = zeros(2,1);
+jdqd(1) = -l_arm*cos(q(2))*qd(2)*qd(2) + (l_arm*cos(q(3)) + h_knuckle*cos(q(3)+q(4)))*qd(3)*qd(3) + 2*h_knuckle*cos(q(3)+q(4))*qd(3)*qd(4) + h_knuckle*cos(q(3)+q(4))*qd(4)*qd(4);
+jdqd(2) = -l_arm*sin(q(2))*qd(2)*qd(2) + (l_arm*sin(q(3)) + h_knuckle*sin(q(3)+q(4)))*qd(3)*qd(3) + 2*h_knuckle*sin(q(3)+q(4))*qd(3)*qd(4) + h_knuckle*sin(q(3)+q(4))*qd(4)*qd(4);
+
+end
