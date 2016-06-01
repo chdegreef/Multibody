43651d73c3c121218fcf6d1721599e2f43b695af
Chdegreef <christophe.degreef@student.uclouvain.be>
2016-06-01 17:34:59 +0200

    With plots

--- PlotsPlotsPlots.m
+++ PlotsPlotsPlots.m
@@ -4,9 +4,9 @@ figure1 = figure;
 axes1 = axes('Parent',gcf,'FontSize',18);
 box(axes1,'on');
 hold(axes1,'on');
-plot(tRobotran,qRobotran(:,2)*180/pi,'DisplayName','Résultats de Robotran','LineWidth',2,'Color',[0 0 1]);
+plot(tRobotran,qRobotran(:,2)*180/pi,'DisplayName','Résultats de Robotran','LineWidth',2.5,'Color',[0 0 1]);
 hold on;
-plot(tSimu,qSimu(:,2)*180/pi,'DisplayName','Résultats du projet','LineWidth',2,'LineStyle','--','Color',[1 0 0]);
+plot(tSimu,qSimu(:,2)*180/pi,'DisplayName','Résultats du projet','LineWidth',2.5,'LineStyle','--','Color',[1 0 0]);
 xlabel('Time [s]','FontSize',18);
 ylabel('Angle du bras supérieur [°]','LineWidth',2,'FontSize',18);
 legend1 = legend(axes1,'show');
@@ -14,15 +14,16 @@ set(legend1,'Location','SouthEast');
 set(figure1, 'Position', [0 0 1500 600]);
 set(figure1, 'PaperPositionMode', 'auto');
 ylim([-30 -12]);
-print -depsc -r300 Plots1.eps 
+xlim([0 5]);
+print -depsc -r300 bras.eps 
 %% ZOOM
 figure1 = figure;
 axes1 = axes('Parent',gcf,'FontSize',18);
 box(axes1,'on');
 hold(axes1,'on');
-plot(tRobotran,qRobotran(:,2)*180/pi,'DisplayName','Résultats de Robotran','LineWidth',2,'Color',[0 0 1]);
+plot(tRobotran,qRobotran(:,2)*180/pi,'DisplayName','Résultats de Robotran','LineWidth',2.5,'Color',[0 0 1]);
 hold on;
-plot(tSimu,qSimu(:,2)*180/pi,'DisplayName','Résultats du projet','LineWidth',2,'Color',[1 0 0]);
+plot(tSimu,qSimu(:,2)*180/pi,'DisplayName','Résultats du projet','LineWidth',2.5,'Color',[1 0 0]);
 xlabel('Time [s]','FontSize',18);
 ylabel('Angle du bras supérieur [°]','LineWidth',2,'FontSize',18);
 legend1 = legend(axes1,'show');
@@ -31,20 +32,21 @@ set(figure1, 'Position', [0 0 1500 600]);
 set(figure1, 'PaperPositionMode', 'auto');
 ylim([-31 -12]);
 xlim([0 0.5]);
-print -depsc -r300 Plots2.eps 
+print -depsc -r300 zoomBras.eps 
 
 %% Hauteur du chassis
 figure1 = figure;
 axes1 = axes('Parent',gcf,'FontSize',18);
 box(axes1,'on');
 hold(axes1,'on');
-plot(tRobotran,qRobotran(:,1),'DisplayName','Résultats de Robotran','LineWidth',2,'Color',[0 0 1]);
+plot(tRobotran,qRobotran(:,1),'DisplayName','Résultats de Robotran','LineWidth',2.5,'Color',[0 0 1]);
 hold on;
-plot(tSimu,qSimu(:,1),'DisplayName','Résultats du projet','LineWidth',2,'LineStyle','--','Color',[1 0 0]);
+plot(tSimu,qSimu(:,1),'DisplayName','Résultats du projet','LineWidth',2.5,'LineStyle','--','Color',[1 0 0]);
 xlabel('Time [s]','FontSize',18);
 ylabel('Hauteur du chassis [m]','LineWidth',2,'FontSize',18);
 legend1 = legend(axes1,'show');
 set(legend1,'Location','SouthEast');
 set(figure1, 'Position', [0 0 1500 600]);
 set(figure1, 'PaperPositionMode', 'auto');
-print -depsc -r300 Plots3.eps 
+xlim([0 5]);
+print -depsc -r300 chassis.eps 
