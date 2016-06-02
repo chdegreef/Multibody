010a3f8fef5fb747432c2a0adda5c4845a183010
Chdegreef <christophe.degreef@student.uclouvain.be>
2016-06-02 19:00:11 +0200

    Rename variables in hJ djdq

--- cons_hJ.m
+++ cons_hJ.m
@@ -2,21 +2,21 @@ function [h,J] = cons_hJ(data)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 l_arm = 0.5;
-h_arm = 0.25;
+h_knuckle = 0.25;
 off_arm = -0.25;
 q = data.q;
 
 %% Constraints
 h = zeros(2,1);
-h(1) = l_arm*cos(q(3)) - h_arm*sin(q(3)+q(4)) - l_arm*cos(q(2));
-h(2) = l_arm*sin(q(3)) + h_arm*cos(q(3)+q(4)) - l_arm*sin(q(2)) + off_arm;
+h(1) = l_arm*cos(q(3)) - h_knuckle*sin(q(3)+q(4)) - l_arm*cos(q(2));
+h(2) = l_arm*sin(q(3)) + h_knuckle*cos(q(3)+q(4)) - l_arm*sin(q(2)) + off_arm;
 
 %% Jacobian
 J = zeros(2,6);
 J(1,2) = l_arm*sin(q(2));
-J(1,3) = -(l_arm*sin(q(3)) + h_arm*cos(q(3)+q(4)));
-J(1,4) = -h_arm*cos(q(3)+q(4));
+J(1,3) = -(l_arm*sin(q(3)) + h_knuckle*cos(q(3)+q(4)));
+J(1,4) = -h_knuckle*cos(q(3)+q(4));
 J(2,2) = -l_arm*cos(q(2));
-J(2,3) = l_arm*cos(q(3)) - h_arm*sin(q(3)+q(4));
-J(2,4) = -h_arm*sin(q(3)+q(4));
+J(2,3) = l_arm*cos(q(3)) - h_knuckle*sin(q(3)+q(4));
+J(2,4) = -h_knuckle*sin(q(3)+q(4));
 end
--- cons_jdqd.m
+++ cons_jdqd.m
@@ -1,11 +1,11 @@
 function jdqd = cons_jdqd(data)
 l_arm = 0.5;
-h_arm = 0.25;
+h_knuckle = 0.25;
 
 q = data.q;
 qd = data.qd;
 jdqd = zeros(2,1);
-jdqd(1) = l_arm*cos(q(2))*qd(2)*qd(2) - l_arm*cos(q(3))*qd(3)*qd(3) + h_arm*sin(q(3)+q(4))*(qd(3)+qd(4))^2;
-jdqd(2) = l_arm*sin(q(2))*qd(2)*qd(2) - l_arm*sin(q(3))*qd(3)*qd(3) - h_arm*cos(q(3)+q(4))*(qd(3)+qd(4))^2;
+jdqd(1) = l_arm*cos(q(2))*qd(2)*qd(2) - l_arm*cos(q(3))*qd(3)*qd(3) + h_knuckle*sin(q(3)+q(4))*(qd(3)+qd(4))^2;
+jdqd(2) = l_arm*sin(q(2))*qd(2)*qd(2) - l_arm*sin(q(3))*qd(3)*qd(3) - h_knuckle*cos(q(3)+q(4))*(qd(3)+qd(4))^2;
 
 end
